2648faa05a5e47166752136da09fcb750ebbdfe3
Nikolay <staroverov2000@yandex.ru>
2023-10-08 09:54:18 +0300

    Имя папки перенесено в метод создания, заголовки (headers) убраны из вызывающего метода

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ class VKAPIClient:
         def get_common_params(self):
                 return {'access_token': self.token, 'v': '5.150', 'owner_id': self.user_id, 'album_id': 'wall', 'extended': 1}
 
-        def _build_url(self, apimethod):
-                return f'{self.API_BASE_url}/{apimethod}'
-
         def get_photos(self):
                 params = self.get_common_params()
-                response = requests.get(self._build_url('photos.get'), params=params)
+                response = requests.get(self.API_BASE_url+'photos.get', params=params)
                 photos_data = response.json()['response']
                 number_of_photos = photos_data['count']
                 if not os.path.exists('Photos from VK'):
                         os.mkdir('Photos from VK')
-                os.chdir(str(os.getcwd()) + str("\Photos from VK"))
+                os.chdir(str(os.getcwd()) + "\Photos from VK")
                 photos_list = []
                 # создание списка словарей фотографий и их URL
                 for i in tqdm(range(number_of_photos), desc='Сохранение файлов с VK на компьютер'):
@@ -39,7 +36,6 @@ class VKAPIClient:
                         number_of_likes = photos_data['items'][i]['likes']['count']
                         # Дата в формате Unixtime
                         date_unixtime = photos_data['items'][i]['date']
-                        date_time_split = []
                         date_time_split = str(datetime.datetime.fromtimestamp(date_unixtime)).split(' ')
                         # все фото загружены в один день и в этом случае сохранится только одна фотография с датой этого дня
                         # поэтому дополнительно в названии файла указывается время
@@ -59,22 +55,20 @@ class VKAPIClient:
 class Yandex_client:
         base_url = "https://cloud-api.yandex.net/v1/disk/resources"
 
-        def __init__(self, token_yandex, folder_name, photos_list):
+        def __init__(self, token_yandex, photos_list):
                 self.token = token_yandex
-                self.folder_name = folder_name
                 self.photos_list = photos_list
 
-        def create_folder(self, headers):
-                params = {'path': self.folder_name}
-                response = requests.put(self.base_url, headers=headers, params=params)
-                return
+        def create_folder(self, folder_name):
+                response = requests.put(self.base_url, {'Authorization': self.token, 'path': folder_name})
 
-        def save_to_yd(self):
+
+        def save_to_yd(self, folder_name):
+                self.create_folder(folder_name)
                 headers = {"Authorization": self.token}
-                self.create_folder(headers)
                 url_for_get_link = self.base_url + "/upload"
                 for i in tqdm(range(len(photos_list)), desc='Запись файло,в на Яндекс Диск'):
-                        params = {'path': self.folder_name + '/'+photos_list[i].get('file_name')}
+                        params = {'path': folder_name + '/'+photos_list[i].get('file_name')}
                         response = requests.get(url_for_get_link, params=params, headers=headers)
                         url_upload = response.json().get('href', '')
                         with open(photos_list[i].get('file_name'), 'rb') as file:
@@ -89,5 +83,5 @@ if __name__ == '__main__':
         token_yd = "OAuth " + tok_yd
         vk_client = VKAPIClient(token, id_vk)
         photos_list = vk_client.get_photos()
-        yd_client = Yandex_client(token_yd, new_folder, photos_list)
-        yd_client.save_to_yd()
+        yd_client = Yandex_client(token_yd, photos_list)
+        yd_client.save_to_yd(new_folder)
